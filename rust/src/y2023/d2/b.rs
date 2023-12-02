6c37cb1cff760e00e222bd9bf5cc517bc36eb78a
Ethan Swan <eswan18@rocketmail.com>
2023-12-02 14:41:01 -0600

    Add 2b in rust.

--- rust/src/y2023/d2/b.rs
+++ rust/src/y2023/d2/b.rs
@@ -1,3 +1,10 @@
-pub fn run_b(_contents: &str) -> Result<String, Box<dyn std::error::Error>> {
-    Ok("b".to_string())
+use super::game::Game;
+
+
+pub fn run_b(contents: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let games = contents.lines().map(|line| Game::new_from_line(line)).collect::<Vec<Game>>();
+    let powers: Vec<i32> = games.iter().map(|game| game.power_of_minimal_draw()).collect();
+
+    // Filter down to games that have are possible from "test_draw"
+    Ok(powers.iter().sum::<i32>().to_string())
 }
--- rust/src/y2023/d2/game.rs
+++ rust/src/y2023/d2/game.rs
@@ -17,4 +17,21 @@ impl Game {
     pub fn possible_from_draw(&self, draw: &Draw) -> bool {
         self.draws.iter().all(|d| d.possible_from_draw(draw))
     }
+
+    fn minimal_draw(&self) -> Draw {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+        for draw in &self.draws {
+            red = std::cmp::max(red, draw.red);
+            green = std::cmp::max(green, draw.green);
+            blue = std::cmp::max(blue, draw.blue);
+        }
+        Draw { red, green, blue }
+    }
+
+    pub fn power_of_minimal_draw(&self) -> i32 {
+        let minimal_draw = self.minimal_draw();
+        minimal_draw.red * minimal_draw.green * minimal_draw.blue
+    }
 }
