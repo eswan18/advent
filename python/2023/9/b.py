d870bfd65e4cf198a166772b49d0f02e7191a276
Ethan Swan <ethanpswan@gmail.com>
2023-12-09 11:23:34 -0600

    Add 9b in Python.

--- python/2023/9/b.py
+++ python/2023/9/b.py
@@ -1,2 +1,7 @@
+from .history import History
+
+
 def b(input: str) -> str:
-    ...
+    histories = [History.build_from_line(line) for line in input.splitlines()]
+    sum_of_prev = sum(history.find_previous() for history in histories)
+    return str(sum_of_prev)
--- python/2023/9/history.py
+++ python/2023/9/history.py
@@ -27,6 +27,26 @@ class History:
             delta_lines[-1].append(next_value)
         # Now we have just one list of deltas, and we can apply it to the main values sequence.
         return self.values[-1] + delta_lines[0][-1]
+    
+    def find_previous(self) -> int:
+        delta_lines = []
+        deltas = self.values
+        # Build out the deltas until they are all equal (the step before they're all 0).
+        while True:
+            deltas = [deltas[i] - deltas[i-1] for i in range(1, len(deltas))]
+            delta_lines.append(deltas)
+            deltas_are_all_equal = len(set(deltas)) == 1
+            if deltas_are_all_equal:
+                break
+        # Now work backwards to find the next value for each list of deltas.
+        while len(delta_lines) > 1:
+            deltas = delta_lines.pop()
+            # solve such that the new first value of the previous deltas is offset from the previous by the first current delta.
+            prev_value = delta_lines[-1][0] - deltas[0]
+            # add to the front of the list
+            delta_lines[-1].insert(0, prev_value)
+        # Now we have just one list of deltas, and we can apply it to the main values sequence.
+        return self.values[0] - delta_lines[0][0]
 
     def __str__(self):
         return ' '.join(str(value) for value in self.values)
