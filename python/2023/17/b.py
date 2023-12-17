621207d3740f3e41d8b8e97bcf796f9d5349149a
Ethan Swan <ethanpswan@gmail.com>
2023-12-17 13:39:35 -0600

    Add 17b in Python.

--- python/2023/17/b.py
+++ python/2023/17/b.py
@@ -1,2 +1,13 @@
+from .grid import Grid, Position
+from .ultra_traversal import UltraTraversal
+
+
 def b(input: str) -> str:
-    ...
+    grid = Grid.from_str(input)
+    traversal = UltraTraversal(
+        start=Position(0, 0),
+        end=Position(grid.width - 1, grid.height - 1),
+        grid=grid,
+    )
+    distance = traversal.find_path()
+    return distance
