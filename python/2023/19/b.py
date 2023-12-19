1bdcaa12630ae0578ab8d0a608577af2e8d4d65c
Ethan Swan <ethanpswan@gmail.com>
2023-12-18 23:47:41 -0600

    Add a too-slow solution to 19b in Python.

--- python/2023/19/b.py
+++ python/2023/19/b.py
@@ -1,2 +1,16 @@
+from itertools import product
+
+from .part import Part
+from .rule import Ruleset
+
+
 def b(input: str) -> str:
-    ...
+    rules_section, parts_section = input.split("\n\n")
+    ruleset = Ruleset.build_from_str(rules_section)
+    count = 0
+    for x, m, a, s in product(range(4000), repeat=4):
+        p = Part(x, m, a, s)
+        if ruleset.route(p):
+            count += 1
+
+    return str(count)
