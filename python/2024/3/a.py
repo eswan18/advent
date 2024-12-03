bd8313e5b3f0248a65d15930a62150790fefb54d
Ethan Swan <ethanpswan@gmail.com>
2024-12-02 23:13:02 -0600

    Add 2024 3 a.

---
+++ python/2024/3/a.py
@@ -0,0 +1,19 @@
+import re
+
+# pattern = re.compile(r'mul((\d\+, \d\+)')
+pattern = re.compile(r'mul\((\d+),(\d+)\)')
+
+def a(input: str) -> str:
+    input = input.strip()
+    pos = 0
+    sum = 0
+    while pos < len(input):
+        if match := pattern.match(input[pos:]):
+            a = int(match.group(1))
+            b = int(match.group(2))
+            sum += a * b
+            pos += match.span()[1]
+        else:
+            pos += 1
+        
+    return str(sum)
