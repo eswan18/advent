109938546f856e90a69e06fa9ced2f5b6813d6dd
Ethan Swan <ethanpswan@gmail.com>
2024-12-07 10:30:53 -0600

    Add 7b.

--- python/2024/7/b.py
+++ python/2024/7/b.py
@@ -1,2 +1,6 @@
+from .threeway_equation import ThreewayEquation
+
 def b(input: str) -> str:
-    ...
+    eqs = [ThreewayEquation.from_str(s) for s in input.splitlines()]
+    solvable_eqs = [eq for eq in eqs if eq.has_solution()]
+    return str(sum(eq.result for eq in solvable_eqs))
