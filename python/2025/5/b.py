d9d41bd7c4e38be74e75a105e993917b400ede0a
Ethan Swan <ethanpswan@gmail.com>
2025-12-04 23:24:46 -0600

    Add 5b.

--- python/2025/5/b.py
+++ python/2025/5/b.py
@@ -1,58 +1,48 @@
 from dataclasses import dataclass
 
-Point = tuple[int, int]
-
 
 @dataclass
-class Grid:
-    width: int
-    height: int
-    paper: set[Point]
+class Range:
+    start: int
+    end: int
 
-    def neighbors(self, p: Point, wraparound: bool = False) -> list[Point]:
-        xs = [p[0] - 1, p[0], p[0] + 1]
-        ys = [p[1] - 1, p[1], p[1] + 1]
-        all_pts = [(x, y) for x in xs for y in ys]
-        # Eliminate the original point
-        all_pts = [pt for pt in all_pts if pt != p]
-        # Eliminate boundary-crossing
-        if not wraparound:
-            all_pts = [pt for pt in all_pts if 0 <= pt[0] < self.width]
-            all_pts = [pt for pt in all_pts if 0 <= pt[1] < self.height]
-        else:
-            # in theory we could allow negative indices as wraparounds but ... nope.
-            raise NotImplementedError
-        return all_pts
+    def contains(self, n: int):
+        return self.start <= n <= self.end
 
     @classmethod
-    def from_str(cls, s: str) -> 'Grid':
-        paper = set()
-        for y, line in enumerate(s.splitlines()):
-            height = y + 1
-            width = len(line)
-            for x, c in enumerate(line):
-                if c == '@':
-                    paper.add((x, y))
-        return cls(width=width, height=height, paper=paper)
+    def from_str(cls, s: str) -> 'Range':
+        parts = s.strip().split('-')
+        if len(parts) != 2:
+            raise ValueError('string must have one `-`')
+        return cls(
+            start=int(parts[0]),
+            end=int(parts[1]),
+        )
 
-def try_remove(grid: Grid) -> set[Point]:
-    accessible = set()
-    for p in grid.paper:
-        neighbors = grid.neighbors(p)
-        paper_neighbors = [n for n in neighbors if n in grid.paper]
-        if len(paper_neighbors) < 4:
-            accessible.add(p)
-    return accessible
+    def size(self) -> int:
+        return self.end - self.start + 1
+
 
 def b(input: str) -> str:
-    grid = Grid.from_str(input)
-    total_removed = 0
-
+    parts = input.strip().split('\n\n')
+    # Sort by the start of each range so we can merge ranges.
+    ranges = sorted(
+        (Range.from_str(line) for line in parts[0].split('\n')),
+        key=lambda r: (r.start, r.end),
+    )
     while True:
-        removable = try_remove(grid)
-        if len(removable) == 0:
+        found_merge = merge_ranges(ranges)
+        if not found_merge:
             break
-        total_removed += len(removable)
-        grid.paper = grid.paper.difference(removable)
 
-    return total_removed
+    return sum(r.size() for r in ranges)
+
+
+def merge_ranges(ranges: list[Range]) -> bool:
+    for i in range(len(ranges)-1):
+        if ranges[i].end >= ranges[i+1].start:
+            ranges[i].end = max(ranges[i].end, ranges[i+1].end)
+            # Remove the range we've now "absorbed"
+            ranges.pop(i+1)
+            return True
+    return False
